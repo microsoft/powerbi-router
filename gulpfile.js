ad742626c0b93d020dd80ef54e31f1e2df7eedbb
Matt Mazzola <mattm@microsoft.com>
2016-08-30 16:15:44 -0700

    Add es6-promise and route-recognizer .d.ts files to sources in gulp docs task.

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,11 @@ gulp.task('ghpages', 'Deploy documentation to gh-pages', ['nojekyll'], function 
 
 gulp.task("docs", 'Compile documentation from src code', function () {
   return gulp
-    .src(["src/**/*.ts"])
+    .src([
+      "typings/globals/es6-promise/index.d.ts",
+      "typings/globals/route-recognizer/index.d.ts",
+      "src/**/*.ts"
+    ])
     .pipe(typedoc({
       mode: 'modules',
       includeDeclarations: true,
